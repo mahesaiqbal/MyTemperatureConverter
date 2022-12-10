657a3d43e835480c205266fd8a590ba01b8bef6b
mahesaiqbal <mahesa.iqbal@amarbank.co.id>
2022-12-10 09:24:16 +0700

    Building UI with Compose and manage states

---
+++ app/src/main/java/com/mahesaiqbal/mytemperatureconverter/MainActivity.kt
@@ -0,0 +1,193 @@
+package com.mahesaiqbal.mytemperatureconverter
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mahesaiqbal.mytemperatureconverter.ui.theme.MyTemperatureConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyTemperatureConverterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Column {
+                        StatefulTemperatureInput()
+                        ConverterApp()
+                        TwoWayConverterApp()
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Stateful version
+// Below is an example of implementing Unidirectional Data Flow (UDF) in Jetpack Compose
+@Composable
+fun StatefulTemperatureInput(
+    modifier: Modifier = Modifier
+) {
+    var input by remember { mutableStateOf("") }
+    var output by remember { mutableStateOf("") }
+    
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(R.string.stateful_converter),
+            style = MaterialTheme.typography.h5
+        )
+        OutlinedTextField(
+            value = input, // Showing state
+            label = { Text(stringResource(R.string.enter_celsius)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = { newInput -> // Event to update the state
+                input = newInput // Update state
+                output = convertToFahrenheit(newInput)
+            }
+        )
+        Text(stringResource(R.string.temperature_fahrenheit, output))
+    }
+}
+
+// Stateful with Stateless composable (State Hoisting)
+@Composable
+private fun ConverterApp(
+    modifier: Modifier = Modifier
+) {
+    var input by remember { mutableStateOf("") }
+    var output by remember { mutableStateOf("") }
+
+    Column(modifier) {
+        StatelessTemperatureInput(
+            input = input,
+            output = output,
+            onValueChange = { newInput ->
+                input = newInput
+                output = convertToFahrenheit(newInput)
+            }
+        )
+    }
+}
+
+@Composable
+fun StatelessTemperatureInput(
+    input: String,
+    output: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(R.string.stateless_converter),
+            style = MaterialTheme.typography.h5
+        )
+        OutlinedTextField(
+            value = input,
+            label = { Text(stringResource(R.string.enter_celsius)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = onValueChange
+        )
+        Text(stringResource(R.string.temperature_fahrenheit, output))
+    }
+}
+
+// General stateless composable for temperature input
+@Composable
+fun GeneralTemperatureInput(
+    scale: Scale,
+    input: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier) {
+        OutlinedTextField(
+            value = input,
+            label = { Text(stringResource(R.string.enter_temperature, scale.scaleName)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = onValueChange
+        )
+    }
+}
+
+// Two way stateful composable by calling stateless general stateless composable
+@Composable
+private fun TwoWayConverterApp(
+    modifier: Modifier = Modifier
+) {
+    // Use rememberSaveable (For example), to maintain the state when the screen orientation changes
+    var celsius by rememberSaveable { mutableStateOf("") }
+    var fahrenheit by rememberSaveable { mutableStateOf("") }
+
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(R.string.two_way_converter),
+            style = MaterialTheme.typography.h5
+        )
+        GeneralTemperatureInput(
+            scale = Scale.CELSIUS,
+            input = celsius,
+            onValueChange = { newInput ->
+                celsius = newInput
+                fahrenheit = convertToFahrenheit(newInput)
+            }
+        )
+        GeneralTemperatureInput(
+            scale = Scale.FAHRENHEIT,
+            input = fahrenheit,
+            onValueChange = { newInput ->
+                fahrenheit = newInput
+                celsius = convertToCelsius(newInput)
+            }
+        )
+    }
+}
+
+// Helper
+private fun convertToFahrenheit(celsius: String) =
+    celsius.toDoubleOrNull()?.let {
+        (it * 9 / 5) + 32
+    }.toString()
+
+private fun convertToCelsius(fahrenheit: String) =
+    fahrenheit.toDoubleOrNull()?.let {
+        (it - 32) * 5 / 9
+    }.toString()
+
+enum class Scale(val scaleName: String) {
+    CELSIUS("Celsius"),
+    FAHRENHEIT("Fahrenheit")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyTemperatureConverterTheme {
+        Column {
+            StatefulTemperatureInput()
+            ConverterApp()
+            TwoWayConverterApp()
+        }
+    }
+}
